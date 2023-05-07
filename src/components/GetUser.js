53e08a5204497a33db0a47c64739b9157b2abbc9
AdnanOlarmmi <olarmmi@gmail.com>
2023-05-07 22:30:08 +0100

    Try allorigin

--- src/components/GetUser.js
+++ src/components/GetUser.js
@@ -16,7 +16,7 @@ function GetUser() {
   const [bio, setBio] = useState(defaultBio);
 
   const getUserBio = async (username = 'olarmmi') => {
-    const response = await fetch(`https://crossorigin.me/https://torre.bio/api/bios/${username}`);
+    const response = await fetch(`https://api.allorigins.win/get?url=/https://torre.bio/api/bios/${username}`);
     const data = await response.json();
     setBio({
       userBio: data,
