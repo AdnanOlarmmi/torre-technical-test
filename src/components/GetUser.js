b86584420046600350ae644182b1606ac976d871
AdnanOlarmmi <olarmmi@gmail.com>
2023-05-07 22:26:39 +0100

    Try cross origin

--- src/components/GetUser.js
+++ src/components/GetUser.js
@@ -16,7 +16,7 @@ function GetUser() {
   const [bio, setBio] = useState(defaultBio);
 
   const getUserBio = async (username = 'olarmmi') => {
-    const response = await fetch(`https://torre.bio/api/bios/${username}`);
+    const response = await fetch(`https://crossorigin.me/https://torre.bio/api/bios/${username}`);
     const data = await response.json();
     setBio({
       userBio: data,
