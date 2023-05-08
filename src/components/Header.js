58bc7aed52892b3dfa564c96d30a6bedf37496ad
AdnanOlarmmi <olarmmi@gmail.com>
2023-05-08 23:04:46 +0100

    Complete header styling

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import getUserBio from '../api/getUserBio';
 import '../styles/Header.css';
@@ -13,10 +13,45 @@ const Header = () => {
       dispatch(getUserBio(username));
     }
   }, [status, dispatch, username]);
+  const searchRef = useRef(null);
+  const formRef = useRef(null);
+  const searchIconRef = useRef(null);
+  const [searchActive, setSearchActive] = useState(false);
+
+  const handleClick = () => {
+    setSearchActive(true);
+    searchIconRef.current.classList.add('hide');
+    formRef.current.classList.remove('hide');
+    formRef.current.focus();
+  };
+
+  const handleClickOutside = (event) => {
+    if (
+      searchRef.current
+      && !searchRef.current.contains(event.target)
+      && event.target !== formRef.current
+      && event.target.tagName.toLowerCase() !== 'input'
+      && event.target.tagName.toLowerCase() !== 'button'
+    ) {
+      setSearchActive(false);
+      searchIconRef.current.classList.remove('hide');
+      formRef.current.classList.add('hide');
+    }
+  };
   const handleSubmit = (e) => {
     e.preventDefault();
     dispatch(getUserBio(username));
+    setSearchActive(false);
+    searchIconRef.current.classList.remove('hide');
+    formRef.current.classList.add('hide');
   };
+  useEffect(() => {
+    document.addEventListener('click', handleClickOutside);
+
+    return () => {
+      document.removeEventListener('click', handleClickOutside);
+    };
+  }, []);
   return (
     <header>
       <span className="material-symbols-outlined header__hamburger btn--white--pointer">
@@ -26,18 +61,21 @@ const Header = () => {
         torre
         <span>.co</span>
       </h4>
-      <form onSubmit={handleSubmit}>
-        <input
-          value={username}
-          onChange={(e) => {
-            setUsername(e.target.value);
-          }}
-        />
-      </form>
-      <span className="material-symbols-outlined header__search btn--white--pointer">
+      <div ref={searchRef}>
+        <form ref={formRef} onSubmit={handleSubmit} className={`${searchActive || 'hide'} header__form`}>
+          <input
+            value={username}
+            placeholder="Search by username..."
+            onChange={(e) => {
+              setUsername(e.target.value);
+            }}
+          />
+        </form>
+      </div>
+      <button type="button" ref={searchIconRef} onClick={handleClick} className="material-symbols-outlined header__search btn--white--pointer">
         search
-      </span>
-      <p className="header__signin btn--white--pointer">SIGN IN</p>
+      </button>
+      <span className="header__signin btn--white--pointer">SIGN IN</span>
     </header>
   );
 };
