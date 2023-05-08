d76a8fe295f705a30f751efb33e45f906e807d0f
AdnanOlarmmi <olarmmi@gmail.com>
2023-05-08 14:56:19 +0100

    dispatch payload

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,9 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useDispatch } from 'react-redux';
 
 const Form = () => {
+  const [username, setUsername] = useState('');
   console.log('lsd');
+  const dispatch = useDispatch();
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    dispatch({ type: 'ADD_USER', payload: username });
+  };
   return (
-    <div>Form</div>
+    <div>
+      <form onSubmit={handleSubmit}>
+        <input
+          value={username}
+          onChange={(e) => {
+            setUsername(e.target.value);
+          }}
+        />
+      </form>
+    </div>
   );
 };
 
