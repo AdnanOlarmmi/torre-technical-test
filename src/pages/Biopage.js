7c33a3846035a09bc13c7fc122c4235c8f480ff4
AdnanOlarmmi <olarmmi@gmail.com>
2023-05-08 21:25:43 +0100

    Dynamic favicon

--- src/pages/Biopage.js
+++ src/pages/Biopage.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
 import skillsArranged from '../helperFxns/skillsArranged';
+import handleFaviconChange from '../helperFxns/handleFavicon';
 
 const Biopage = () => {
   const proficiencyLevels = ['master', 'expert', 'proficient', 'beginner', 'no-experience-interested'];
   const state = useSelector((state) => state);
   const { bio } = state;
   if (bio?.person?.name) {
+    handleFaviconChange(bio.person.picture);
     return (
       <div>
         { bio.person.name }
