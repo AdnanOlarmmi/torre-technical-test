defaf44d160b5b5892fb888c63b2180d7241f123
AdnanOlarmmi <olarmmi@gmail.com>
2023-05-09 02:17:05 +0100

    Add selected strength to skill

--- src/pages/Biopage.js
+++ src/pages/Biopage.js
@@ -1,5 +1,6 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useSelector } from 'react-redux';
+import StrengthContext from '../helperFxns/StrengthContext';
 import skillsArranged from '../helperFxns/skillsArranged';
 import handleFaviconChange from '../helperFxns/handleFavicon';
 import '../styles/Biopage.css';
@@ -7,20 +8,23 @@ import '../styles/Biopage.css';
 const Biopage = () => {
   const proficiencyLevels = ['master', 'expert', 'proficient', 'beginner', 'no-experience-interested'];
   const state = useSelector((state) => state);
+  const [selectedStrength, setSelectedStrength] = useState(null);
   const { bio } = state;
   if (bio?.person?.name) {
     handleFaviconChange(bio.person.picture);
     return (
-      <div className="bio__container">
-        <div className="bio__image-container">
-          <img src={bio.person.picture} alt={bio.person.name} />
+      <StrengthContext.Provider value={{ selectedStrength, setSelectedStrength }}>
+        <div className="bio__container">
+          <div className="bio__image-container">
+            <img src={bio.person.picture} alt={bio.person.name} />
+          </div>
+          <h3 className="bio__username">{ bio.person.name }</h3>
+          <section>
+            <h4 className="bio__heading">Skills and Interests:</h4>
+            {skillsArranged(bio.strengths, proficiencyLevels)}
+          </section>
         </div>
-        <h3 className="bio__username">{ bio.person.name }</h3>
-        <section>
-          <h4 className="bio__heading">Skills and Interests:</h4>
-          {skillsArranged(bio.strengths, proficiencyLevels)}
-        </section>
-      </div>
+      </StrengthContext.Provider>
     );
   } return null;
 };
--- src/pages/SkillsInfo.js
+++ src/pages/SkillsInfo.js
@@ -1,39 +1,11 @@
-import React from 'react';
-import PropTypes from 'prop-types';
+import React, { useContext } from 'react';
+import StrengthContext from '../helperFxns/StrengthContext';
 import '../styles/SkillsInfo.css';
 
-const SkillsInfo = ({ newStrength }) => {
-  if (newStrength.length) {
-    const skill = newStrength[0];
-    return (
-      <div className="skillInfo">
-        <div className="header">
-          { skill.name }
-        </div>
-        <div>
-          Proficiency:
-          { skill.proficiency.replace(/-/g, ' ') }
-          <br />
-          Recommendations:
-          { skill.recommendations }
-        </div>
-
-      </div>
-    );
-  }
-  return null;
-};
-
-SkillsInfo.propTypes = {
-  newStrength: PropTypes.arrayOf(
-    PropTypes.shape({
-      id: PropTypes.string.isRequired,
-      name: PropTypes.string.isRequired,
-      proficiency: PropTypes.string.isRequired,
-      weight: PropTypes.number.isRequired,
-      recommendations: PropTypes.number.isRequired,
-    }),
-  ).isRequired,
+const SkillsInfo = () => {
+  const { selectedStrength } = useContext(StrengthContext);
+  console.log(selectedStrength);
+  return (<div>as</div>);
 };
 
 export default SkillsInfo;
