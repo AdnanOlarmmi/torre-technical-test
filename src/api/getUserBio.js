23686869830789aee1a0d40dd8df89bd709322c9
AdnanOlarmmi <olarmmi@gmail.com>
2023-05-09 17:55:49 +0100

    Switch back to correct server

--- src/api/getUserBio.js
+++ src/api/getUserBio.js
@@ -2,7 +2,7 @@ import { createAsyncThunk } from '@reduxjs/toolkit';
 
 const getUserBio = createAsyncThunk('bio/fetchBio', async (username) => {
   try {
-    const response = await fetch(`http://localhost:8080/bios/${username}`);
+    const response = await fetch(`https://enchanting-melba-5fb712.netlify.app/.netlify/functions/server/bios/${username}`);
     const data = await response.json();
     return data;
   } catch (error) {
